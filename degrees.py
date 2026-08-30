4fbb01efea2921affad1d5c0c69ed50d857f617c
WisdomOgebe <wisdommichael501@gmail.com>
2026-08-30 22:37:21 +0100

    Added code to check if source is target. Changed 'return None' to 'return []'

--- degrees.py
+++ degrees.py
@@ -98,7 +98,7 @@ def shortest_path(source, target):
 
     # TODO
     if source == target:
-        return None
+        return []
     q_frontier = QueueFrontier()
     explored = {source}
     for neighbor in neighbors_for_person(source):
